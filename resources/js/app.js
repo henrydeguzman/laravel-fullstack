21d55c8dfd697ea4e9fda0eb130746a095dad981
henry <henrydeguzman.java73@gmail.com>
2020-10-19 20:20:38 +0800

    updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+// Get vue
+window.Vue = require('vue');
+
+// Add Components
+Vue.component('mainapp', require('./components/mainapp.vue').default)
+
+// Load the Vue
+const app = new Vue({
+    el: '#app'
+})
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/new', [TestController::class, 'controllerMethod']);
+
+// Catch any routes does not match above
+Route::any('{slug}', function(){
     return view('welcome');
 });
